b3ea3908ac8dfa6ba79bd43ce22aed92233fa437
andreiggar <andreiggar@gmail.com>
2023-04-18 14:21:25 +0200

    Commit

--- MvcApiManagement/Program.cs
+++ MvcApiManagement/Program.cs
@@ -1,6 +1,9 @@
+using MvcApiManagement.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddTransient<ServiceApiManagement>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
